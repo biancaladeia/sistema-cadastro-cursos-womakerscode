0cc4d217061be11c09555028e5c5dcbcc2d2552a
biancaladeia <biancaladeia@icloud.com>
2023-10-17 20:40:03 -0300

    aula 6

--- cadastro_curso_womakers/base/models.py
+++ cadastro_curso_womakers/base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=50)
+    data = models.DateTimeField(auto_now_add=True)
+    
