9d0219f744188425f3c98d902a1757ea83b6a43a
biancaladeia <biancaladeia@icloud.com>
2023-10-17 16:18:05 -0300

    aula 3

--- cadastro_curso_womakers/base/views.py
+++ cadastro_curso_womakers/base/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("Olá, mundo. Você está na página inicial do projeto womakerscode!")
+    html = '''
+    <!DOCTYPE html>
+        <head>
+            <title>Cursos Online</title>
+        </head>
+        <body>
+            <h1> Olá mundo!</h1>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
+
 
 def cadastro(request):
     pass
