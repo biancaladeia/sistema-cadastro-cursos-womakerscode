e9995112cd25aa3c9d83c51190243970e39700b9
biancaladeia <biancaladeia@icloud.com>
2023-10-20 17:06:48 -0300

    update urls.py.cursos

--- cadastro_curso_womakers/cursos/urls.py
+++ cadastro_curso_womakers/cursos/urls.py
@@ -3,5 +3,5 @@ from cursos.views import criar_curso
 
 app_name = 'Cursos'
 urlpatterns = [
-    path('criar_curso', criar_curso, name='criar_curso')
+    path('criar_curso/', criar_curso, name='criar_curso')
 ]
